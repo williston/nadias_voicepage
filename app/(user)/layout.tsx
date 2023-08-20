4845d66a0609c12e7813282cf29fe3e099981527
John Harris <willistonharris@gmail.com>
2023-08-19 21:21:55 -0700

    removal overflow scroll from layout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -11,8 +11,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-        <div className='bg-slate-100 w-full max-w-screen-sm md:max-w-7xl mx-auto overflow-hidden'>
-        <Header2/>
+        <div className='bg-slate-100 w-full max-w-screen-sm md:max-w-7xl mx-auto'>\
+        <Header2 />
+        
         {children}
         </div>
   )
